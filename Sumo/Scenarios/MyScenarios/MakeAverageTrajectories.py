13d215ae3762538cecc067d197bfdac1e929527a
Felix <f.rampf@tum.de>
2020-04-28 00:15:53 +0200

    Removed last hardcoded paths

--- Sumo/Scenarios/MyScenarios/MakeAverageTrajectories.py
+++ Sumo/Scenarios/MyScenarios/MakeAverageTrajectories.py
@@ -5,15 +5,16 @@ import Sumo.SumoHelperFcts as shf
 network_settings = params.NETWORK("ValidationNetwork.net.xml")
 paths = params.SUMO_PATHS()
 
+SCENARIO_PATH = params.SUMO_PATHS().scenarios
 
 used_scenario = "1a"
 test_scenario = shf.Scenario(used_scenario, network_settings.net)
 test_scenario.import_scenario()
 _, dataset = test_scenario.get_average_trajectory(add_header=False)
 # save to csv file
-dataset.to_csv(r'C:\Users\Felix\Masterarbeit\Coding\Sumo\Scenarios\MyScenarios\Scenario2\tum_data.csv',
+dataset.to_csv(SCENARIO_PATH +  r'\MyScenarios\Scenario2\tum_data.csv',
                index=False, header=True)
-dataset.to_csv(r'C:\Users\Felix\Masterarbeit\Coding\Sumo\Scenarios\MyScenarios\Scenario3\tum_data.csv',
+dataset.to_csv(SCENARIO_PATH + r'\MyScenarios\Scenario3\tum_data.csv',
                index=False, header=True)
 
 
@@ -22,7 +23,7 @@ test_scenario = shf.Scenario(used_scenario, network_settings.net)
 test_scenario.import_scenario()
 _, dataset = test_scenario.get_average_trajectory(add_header=False)
 # save to csv file
-dataset.to_csv(r'C:\Users\Felix\Masterarbeit\Coding\Sumo\Scenarios\MyScenarios\Scenario1\tum_data.csv',
+dataset.to_csv(SCENARIO_PATH + r'\MyScenarios\Scenario1\tum_data.csv',
                index=False, header=True)
 
 
@@ -31,7 +32,7 @@ test_scenario = shf.Scenario(used_scenario, network_settings.net)
 test_scenario.import_scenario()
 _, dataset = test_scenario.get_average_trajectory(add_header=False)
 # save to csv file
-dataset.to_csv(r'C:\Users\Felix\Masterarbeit\Coding\Sumo\Scenarios\MyScenarios\Scenario5\tum_data.csv',
+dataset.to_csv(SCENARIO_PATH + r'\MyScenarios\Scenario5\tum_data.csv',
                index=False, header=True)
 
 
@@ -40,7 +41,7 @@ test_scenario = shf.Scenario(used_scenario, network_settings.net)
 test_scenario.import_scenario()
 _, dataset = test_scenario.get_average_trajectory(add_header=False)
 # save to csv file
-dataset.to_csv(r'C:\Users\Felix\Masterarbeit\Coding\Sumo\Scenarios\MyScenarios\Scenario4\tum_data.csv',
+dataset.to_csv(SCENARIO_PATH + r'\MyScenarios\Scenario4\tum_data.csv',
                index=False, header=True)
 
 print("Average Scenario Data Created and Saved!")
